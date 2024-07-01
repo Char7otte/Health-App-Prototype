24367dbd1c34d653ef90c1dedbb279706f7cb739
shar <faethychan@gmail.com>
2024-07-01 13:56:57 +0800

    _delta

--- scenes/home_menu/clock.gd
+++ scenes/home_menu/clock.gd
@@ -2,6 +2,6 @@ extends HBoxContainer
 
 @export var system_time_display_label: Label
 
-func _process(delta):
+func _process(_delta):
 	var system_time = Time.get_time_string_from_system()
 	system_time_display_label.text = system_time
